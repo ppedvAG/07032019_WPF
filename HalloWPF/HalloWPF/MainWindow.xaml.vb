10b302df370d284dfe416127d73418ee77e11abf
André <andrerrr@outlook.de>
2019-03-07 13:13:12 +0100

    HalloWPF

---
+++ HalloWPF/HalloWPF/MainWindow.xaml.vb
@@ -0,0 +1,5 @@
+﻿Class MainWindow
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        meinButton.Content = "lalfeöpfmewlkfmewlkfmlwemflkewmflkmwelkfmewlkfmwelkmfwelkfmewlkfmlma"
+    End Sub
+End Class
