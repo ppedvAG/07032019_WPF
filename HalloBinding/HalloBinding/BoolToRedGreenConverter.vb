d03ea8cdb4a7bf2c254e5878200e65b5c51b0c33
André <andrerrr@outlook.de>
2019-03-07 14:19:50 +0100

    Binding

---
+++ HalloBinding/HalloBinding/BoolToRedGreenConverter.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Globalization
+
+Public Class BoolToRedGreenConverter
+    Implements IValueConverter
+
+    Public Function Convert(value As Object, targetType As Type, parameter As Object, culture As CultureInfo) As Object Implements IValueConverter.Convert
+
+        If CType(value, Boolean) Then
+            Return New SolidColorBrush(Colors.Green)
+        Else
+            Return New SolidColorBrush(Colors.Red)
+        End If
+
+    End Function
+
+    Public Function ConvertBack(value As Object, targetType As Type, parameter As Object, culture As CultureInfo) As Object Implements IValueConverter.ConvertBack
+        Throw New NotImplementedException()
+    End Function
+End Class
