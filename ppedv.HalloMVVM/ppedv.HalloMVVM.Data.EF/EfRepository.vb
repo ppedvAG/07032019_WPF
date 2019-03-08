87b2e83db875bac1fccbe30f79cd353a8744a7cd
André <andrerrr@outlook.de>
2019-03-08 12:23:06 +0100

    Generisches Repository implementiert

---
+++ ppedv.HalloMVVM/ppedv.HalloMVVM.Data.EF/EfRepository.vb
@@ -0,0 +1,40 @@
+﻿Imports ppedv.HalloMVVM.Model
+
+Public Class EfRepository
+    Implements IRepository
+
+    Dim con As New EfContext()
+
+    Public Sub Add(Of T As Entity)(entity As T) Implements IRepository.Add
+        con.Set(Of T)().Add(entity)
+    End Sub
+
+    Public Sub Delete(Of T As Entity)(entity As T) Implements IRepository.Delete
+        con.Set(Of T)().Remove(entity)
+    End Sub
+
+    Public Sub Update(Of T As Entity)(entity As T) Implements IRepository.Update
+        'nur für "Disconnected access" wie WCF,ASP...
+        'wird nicht für desktop WinForms oder WPF benötigt
+        Dim loaded = GetById(Of T)(entity.Id)
+        If Not loaded Is Nothing Then
+            con.Entry(loaded).CurrentValues.SetValues(entity)
+        End If
+    End Sub
+
+    Public Sub SaveAll() Implements IRepository.SaveAll
+        con.SaveChanges()
+    End Sub
+
+    Public Function GetAll(Of T As Entity)() As IEnumerable(Of T) Implements IRepository.GetAll
+        Return con.Set(Of T)().ToList()
+    End Function
+
+    Public Function GetById(Of T As Entity)(id As Integer) As T Implements IRepository.GetById
+        Return con.Set(Of T)().Find(id)
+    End Function
+
+    Public Function Query(Of T As Entity)() As IQueryable(Of T) Implements IRepository.Query
+        Return con.Set(Of T)()
+    End Function
+End Class
---
+++ ppedv.HalloMVVM/ppedv.HalloMVVM.Model/Contracts/IRepository.vb
@@ -0,0 +1,11 @@
+﻿Public Interface IRepository
+
+    Function GetAll(Of T As Entity)() As IEnumerable(Of T)
+    Function GetById(Of T As Entity)(id As Integer) As T
+    Function Query(Of T As Entity)() As IQueryable(Of T)
+    Sub Add(Of T As Entity)(entity As T)
+    Sub Delete(Of T As Entity)(entity As T)
+    Sub Update(Of T As Entity)(entity As T)
+    Sub SaveAll()
+
+End Interface
