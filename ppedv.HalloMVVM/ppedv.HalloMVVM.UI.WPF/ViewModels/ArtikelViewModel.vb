30fcff22cbbdd2868069ae7ac233686b3eb07f3e
André <andrerrr@outlook.de>
2019-03-08 16:07:47 +0100

    MVVM Fun

--- ppedv.HalloMVVM/ppedv.HalloMVVM.UI.WPF/ViewModels/ArtikelViewModel.vb
+++ ppedv.HalloMVVM/ppedv.HalloMVVM.UI.WPF/ViewModels/ArtikelViewModel.vb
@@ -3,10 +3,50 @@ Imports ppedv.HalloMVVM.Logic
 Imports ppedv.HalloMVVM.Model
 
 Public Class ArtikelViewModel
+    Inherits ViewModelBase
 
     Public Property ArtikelList As ObservableCollection(Of Artikel) = New ObservableCollection(Of Artikel)()
 
-    Public Property SelectedArtikel As Artikel
+    Private _selectedArtikel As Artikel
+    Public Property SelectedArtikel() As Artikel
+        Get
+            Return _selectedArtikel
+        End Get
+        Set(ByVal value As Artikel)
+            _selectedArtikel = value
+
+            OnPropertyChanged(NameOf(SelectedArtikel))
+            OnPropertyChanged(NameOf(RabattPreis))
+
+        End Set
+    End Property
+
+    Public Property Preis() As Decimal
+        Get
+            If SelectedArtikel Is Nothing Then
+                Return -999
+            End If
+            Return SelectedArtikel.Preis
+        End Get
+        Set(ByVal value As Decimal)
+            If Not SelectedArtikel Is Nothing Then
+                SelectedArtikel.Preis = value
+                OnPropertyChanged(NameOf(Preis))
+                OnPropertyChanged(NameOf(RabattPreis))
+                AllPropertiesChanged()
+            End If
+        End Set
+    End Property
+
+
+    Public ReadOnly Property RabattPreis As Decimal
+        Get
+            If SelectedArtikel Is Nothing Then
+                Return -999
+            End If
+            Return SelectedArtikel.Preis * 0.2D
+        End Get
+    End Property
 
     Dim core As New Core()
 
---
+++ ppedv.HalloMVVM/ppedv.HalloMVVM.UI.WPF/ViewModels/ViewModelBase.vb
@@ -0,0 +1,15 @@
+﻿Imports System.ComponentModel
+
+Public MustInherit Class ViewModelBase
+    Implements INotifyPropertyChanged
+
+    Public Event PropertyChanged As PropertyChangedEventHandler Implements INotifyPropertyChanged.PropertyChanged
+
+    Protected Sub OnPropertyChanged(propName As String)
+        RaiseEvent PropertyChanged(Me, New PropertyChangedEventArgs(propName))
+    End Sub
+
+    Protected Sub AllPropertiesChanged()
+        OnPropertyChanged("")
+    End Sub
+End Class
