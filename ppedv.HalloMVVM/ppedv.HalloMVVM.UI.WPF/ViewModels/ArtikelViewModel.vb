cfa5ebca5b2e8012cc112b064370768cb3ec021a
André <andrerrr@outlook.de>
2019-03-08 16:24:44 +0100

    ArtikelItem(ViewModel) <=> Wrapper für Artikel (DomainModel)

--- ppedv.HalloMVVM/ppedv.HalloMVVM.UI.WPF/ViewModels/ArtikelViewModel.vb
+++ ppedv.HalloMVVM/ppedv.HalloMVVM.UI.WPF/ViewModels/ArtikelViewModel.vb
@@ -5,54 +5,91 @@ Imports ppedv.HalloMVVM.Model
 Public Class ArtikelViewModel
     Inherits ViewModelBase
 
-    Public Property ArtikelList As ObservableCollection(Of Artikel) = New ObservableCollection(Of Artikel)()
+    Public Property ArtikelList As ObservableCollection(Of ArtikelItem) = New ObservableCollection(Of ArtikelItem)()
 
-    Private _selectedArtikel As Artikel
-    Public Property SelectedArtikel() As Artikel
+    Private _selectedArtikel As ArtikelItem
+    Public Property SelectedArtikel() As ArtikelItem
         Get
             Return _selectedArtikel
         End Get
-        Set(ByVal value As Artikel)
+        Set(ByVal value As ArtikelItem)
             _selectedArtikel = value
 
             OnPropertyChanged(NameOf(SelectedArtikel))
-            OnPropertyChanged(NameOf(RabattPreis))
 
+        End Set
+    End Property
+
+    'Public Property Preis() As Decimal
+    '    Get
+    '        If SelectedArtikel Is Nothing Then
+    '            Return -999
+    '        End If
+    '        Return SelectedArtikel.Preis
+    '    End Get
+    '    Set(ByVal value As Decimal)
+    '        If Not SelectedArtikel Is Nothing Then
+    '            SelectedArtikel.Preis = value
+    '            OnPropertyChanged(NameOf(Preis))
+    '            OnPropertyChanged(NameOf(RabattPreis))
+    '            AllPropertiesChanged()
+    '        End If
+    '    End Set
+    'End Property
+
+
+
+
+    Dim core As New Core()
+
+    Sub New()
+        ArtikelList = New ObservableCollection(Of ArtikelItem)()
+        LoadItems()
+    End Sub
+
+    Private Sub LoadItems()
+        ArtikelList.Clear()
+        For Each item In core.Repository.GetAll(Of Artikel)()
+            ArtikelList.Add(New ArtikelItem() With {.Artikel = item})
+        Next
+
+    End Sub
+End Class
+
+Public Class ArtikelItem
+    Inherits ViewModelBase
+
+    Property Artikel As Artikel
+
+    Public Property Name() As String
+        Get
+            Return Artikel.Name
+        End Get
+        Set(ByVal value As String)
+            Artikel.Name = value
+            OnPropertyChanged(NameOf(ArtikelItem.Name))
         End Set
     End Property
 
     Public Property Preis() As Decimal
         Get
-            If SelectedArtikel Is Nothing Then
-                Return -999
-            End If
-            Return SelectedArtikel.Preis
+            Return Artikel.Preis
         End Get
         Set(ByVal value As Decimal)
-            If Not SelectedArtikel Is Nothing Then
-                SelectedArtikel.Preis = value
-                OnPropertyChanged(NameOf(Preis))
-                OnPropertyChanged(NameOf(RabattPreis))
-                AllPropertiesChanged()
-            End If
+            Artikel.Preis = value
+            OnPropertyChanged(NameOf(ArtikelItem.Preis))
+            OnPropertyChanged(NameOf(ArtikelItem.RabattPreis))
         End Set
     End Property
 
 
     Public ReadOnly Property RabattPreis As Decimal
         Get
-            If SelectedArtikel Is Nothing Then
+            If Artikel Is Nothing Then
                 Return -999
             End If
-            Return SelectedArtikel.Preis * 0.2D
+            Return Artikel.Preis * 0.2D
         End Get
     End Property
-
-    Dim core As New Core()
-
-    Sub New()
-        ArtikelList = New ObservableCollection(Of Artikel)(core.Repository.GetAll(Of Artikel))
-    End Sub
-
 
 End Class
